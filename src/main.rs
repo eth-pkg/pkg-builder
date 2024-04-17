91d7c9ddb19bb1cac4f469718981d94562653b29
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2024-04-17 11:12:54 +0700

    fix: error code on exit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ mod v1;
 fn main() {
     let result = v1::cli::run_cli();
     match result {
-        Ok(_) => println!("Success"),
-        Err(err) => println!("Failed to run: {:?}", err),
+        Ok(_) => {
+            std::process::exit(0);
+        },
+        Err(err) => {
+            println!("Failed to run: {:?}", err);
+            std::process::exit(1);
+        },
     }
 }
