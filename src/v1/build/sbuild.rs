5dd909da1d4313fa97252e682f5c0b7320139c55
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-01-07 14:44:13 +0100

    fix: autopkgtest

--- src/v1/build/sbuild.rs
+++ src/v1/build/sbuild.rs
@@ -762,6 +762,7 @@ fn check_autopkgtest_version(expected_version: String) -> Result<()> {
             .replace("ubuntu3~22.04.1", "")
             .replace("ubuntu2", "")
             .replace("ubuntu1~24.04.1", "")
+            .replace("ubuntu1~22.04.1", "")
             .trim()
             .to_string();
         if let Some(pos) = output_str.find("all ") {
