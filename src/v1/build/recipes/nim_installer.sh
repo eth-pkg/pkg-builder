557f5877c565596f553f57bfc5c19698c717bd99
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-03-09 12:40:21 +0100

    fix:typo

--- src/v1/build/recipes/nim_installer.sh
+++ src/v1/build/recipes/nim_installer.sh
@@ -5,7 +5,7 @@ cd /tmp && wget -q ${nim_binary_url}
 cd /tmp && echo ${nim_version_checksum} >> hash_file.txt && cat hash_file.txt
 cd /tmp && sha256sum -c hash_file.txt
 cd /tmp && tar xJf nim-${nim_version}-linux_x64.tar.xz -C nim-${nim_version} --strip-components=1
-cd /tmp && mv nim-${nim_version} /opt/lib/ni
+cd /tmp && mv nim-${nim_version} /opt/lib/nim
 ln -s /opt/lib/nim/nim-${nim_version}/bin/nim /usr/bin/nim
 nim --version
 apt remove -y wget
