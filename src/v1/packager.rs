7e23703a00f262ab3af9a0deaf972f1870f2e6df
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-03-09 12:30:47 +0100

    chore: simplify packager

--- src/v1/packager.rs
+++ src/v1/packager.rs
@@ -1,10 +1,7 @@
 use eyre::{eyre, Result};
 use crate::v1::build::sbuild_packager::SbuildPackager;
-
-
 use crate::v1::pkg_config::PkgConfig;
 use crate::v1::pkg_config_verify::PkgVerifyConfig;
-
 
 pub trait Packager {
     type BuildEnv: BackendBuildEnv;
@@ -13,163 +10,81 @@ pub trait Packager {
     fn get_build_env(&self) -> Result<Self::BuildEnv>;
 }
 
-pub struct DistributionPackager {
-    config: PkgConfig,
-    config_root: String,
-}
-
 pub trait BackendBuildEnv {
     fn clean(&self) -> Result<()>;
     fn create(&self) -> Result<()>;
     fn package(&self) -> Result<()>;
-
     fn verify(&self, verify_config: PkgVerifyConfig) -> Result<()>;
-
     fn run_lintian(&self) -> Result<()>;
     fn run_piuparts(&self) -> Result<()>;
     fn run_autopkgtests(&self) -> Result<()>;
 }
 
+pub struct DistributionPackager {
+    config: PkgConfig,
+    config_root: String,
+}
+
 impl DistributionPackager {
     pub fn new(config: PkgConfig, config_root: String) -> Self {
-        DistributionPackager {
-            config,
-            config_root,
+        Self { config, config_root }
+    }
+
+    fn with_packager<F>(&self, operation: F) -> Result<()>
+    where
+        F: Fn(&SbuildPackager) -> Result<()>,
+    {
+        let codename = self.config.build_env.codename.as_str();
+        match codename {
+            "bookworm" | "noble numbat" | "jammy jellyfish" => {
+                let packager = SbuildPackager::new(self.config.clone(), self.config_root.clone());
+                operation(&packager)
+            }
+            _ => Err(eyre!("Invalid codename '{}' specified", codename)),
         }
     }
+
+    fn with_build_env<F>(&self, operation: F) -> Result<()>
+    where
+        F: Fn(&<SbuildPackager as Packager>::BuildEnv) -> Result<()>,
+    {
+        self.with_packager(|packager| {
+            let build_env = packager.get_build_env()?;
+            operation(&build_env)
+        })
+    }
+
     pub fn package(&self) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                packager.package()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+        self.with_packager(|packager| packager.package())
     }
+
     pub fn run_lintian(&self) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                let build_env = packager.get_build_env()?;
-                build_env.run_lintian()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+        self.with_build_env(|env| env.run_lintian())
     }
+
     pub fn run_piuparts(&self) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                let build_env = packager.get_build_env()?;
-                build_env.run_piuparts()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+        self.with_build_env(|env| env.run_piuparts())
     }
+
     pub fn run_autopkgtests(&self) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                let build_env = packager.get_build_env()?;
-                build_env.run_autopkgtests()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+        self.with_build_env(|env| env.run_autopkgtests())
     }
+
     pub fn clean_build_env(&self) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-
-                let build_env = packager.get_build_env()?;
-                build_env.clean()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+        self.with_build_env(|env| env.clean())
     }
-    pub fn create_build_env(&self) -> Result<()> {
-        let config = self.config.clone();
 
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                let build_env = packager.get_build_env()?;
-                build_env.create()?;
-            }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+    pub fn create_build_env(&self) -> Result<()> {
+        self.with_build_env(|env| env.create())
     }
 
     pub fn verify(&self, verify_config: PkgVerifyConfig, package: bool) -> Result<()> {
-        let config = self.config.clone();
-
-        match self.config.build_env.codename.clone().as_str() {
-            "bookworm" | "noble numbat" | "jammy jellyfish" => {
-                let mut config = config.clone();
-                config.build_env.run_autopkgtest = Some(false);
-                config.build_env.run_lintian = Some(false);
-                config.build_env.run_piuparts = Some(false);
-                let packager = SbuildPackager::new(config, self.config_root.clone());
-                if package {
-                    packager.package()?;
-                }
-                let build_env = packager.get_build_env()?;
-                // files to verify
-                build_env.verify(verify_config)?;
+        self.with_packager(|packager| {
+            if package {
+                packager.package()?;
             }
-            invalid_codename => {
-                return Err(eyre!(format!(
-                    "Invalid codename '{}' specified",
-                    invalid_codename
-                )));
-            }
-        }
-        Ok(())
+            let build_env = packager.get_build_env()?;
+            build_env.verify(verify_config.clone())
+        })
     }
 }
